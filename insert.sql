6d241fc65a964eeb85b7967b466f31417796a04b
ssenti <39092585+ssenti@users.noreply.github.com>
2019-04-07 22:28:26 -0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,195 @@
+
+insert into OfficePostalCode values (85001, 'Phoenix');
+insert into OfficePostalCode values (72201, 'Little Rock');
+insert into OfficePostalCode values (94203, 'Sacramento');
+insert into OfficePostalCode values (80201, 'Denver');
+insert into OfficePostalCode values (06101, 'Hartford');
+insert into OfficePostalCode values (19901, 'Dover');
+insert into OfficePostalCode values (20001, 'Washington');
+insert into OfficePostalCode values (32501, 'Pensacola');
+insert into OfficePostalCode values (30301, 'Atlanta');
+insert into OfficePostalCode values (83254, 'Montpelier');
+
+insert into Office values (1, 85001);
+insert into Office values (2, 72201);
+insert into Office values (3, 94203);
+insert into Office values (4, 80201);
+insert into Office values (5, 06101);
+insert into Office values (6, 19901);
+insert into Office values (7, 20001);
+insert into Office values (8, 32501);
+insert into Office values (9, 30301);
+insert into Office values (10, 83254);
+
+insert into Seller values (1, 'Benjamin', 'Chen', 123456788, 'benjaminchen@gmail.com');
+insert into Seller values (2, 'Frank', 'Looi', 223456788, 'franklooi@gmail.com');
+insert into Seller values (3, 'Frances', 'Pak', 223456788, 'francespak@daum.net');
+insert into Seller values (4, 'Christine', 'Tan', 223456788, 'christinetan@gmail.com');
+insert into Seller values (5, 'Max', 'Teh', 533256588, 'maxteh@naver.com');
+insert into Seller values (6, 'Keon Woo', 'Lim', 433456782, 'keonlim@gmail.com');
+insert into Seller values (7, 'Max', 'Millian', 8212456783, 'maxmil@gmail.com');
+insert into Seller values (8, 'Manuel', 'Prito', 5234456732, 'manprito@daum.net');
+insert into Seller values (9, 'Keon Woo', 'Park', 923456780, 'woopark@gmail.com');
+insert into Seller values (10, 'Sungjae', 'Cho', 728446738, 'sungcho@naver.com');
+
+insert into Agent values (1,'Connor', "Paneru", 1234567789, 'connorpaneru@gmail.com', 35, 1220);
+insert into Agent values (2, 'Oren', "Flores", 2234567889, 'orenflores@hotmail.com', 37, 1352);
+insert into Agent values (3, 'Pranav', "Stein", 2234537833, 'pranavstein@gmail.com', 29, 652);
+insert into Agent values (4, 'Sofia', "Chen", 4234516386, 'sofiachen@gmail.com', 52, 4352);
+insert into Agent values (5, 'Liberty', "Kay", 5223522882, 'libertykay@hotmail.com', 23, 152);
+insert into Agent values (6, 'Sandie', "Hagan", 6534537283, 'sandiehagan@gmail.com', 31, 1552);
+insert into Agent values (7,'Galatea', "Panero", 7233567389, 'gpanero@gmail.com', 25, 1220);
+insert into Agent values (8, 'Jennifer', "Kim", 2234267849, 'jennkim@hotmail.com', 57, 3352);
+insert into Agent values (9, 'Sonia', "Pak", 5234537389, 'sonpak@gmail.com', 32, 1652);
+
+insert into ListedHouse values (1, 0, 1, 6, 64102, 32, 4, 1, 874000,'2019-01-27 14:04:05');
+insert into ListedHouse values (2, 0, 2, 3, 84519, 58, 5, 2, 1250000,'2018-04-15 09:39:16');
+insert into ListedHouse values (3, 0, 3, 5, 52619, 12, 6, 2, 142000,'2017-12-18 16:04:09');
+insert into ListedHouse values (4, 0, 4, 4, 94702, 32, 4, 1, 573000,'2018-07-25 17:43:46');
+insert into ListedHouse values (5, 0, 5, 2, 94819, 87, 5, 2, 928000,'2019-03-07 14:29:35');
+insert into ListedHouse values (6, 0, 5, 6, 74549, 42, 4, 2, 570000,'2018-08-20 13:43:52');
+insert into ListedHouse values (7, 0, 6, 1, 43219, 51, 3, 1, 453000,'2019-03-05 19:15:12');
+insert into ListedHouse values (8, 0, 7, 2, 54819, 67, 5, 2, 928000,'2019-03-02 14:29:35');
+insert into ListedHouse values (9, 0, 8, 6, 44549, 72, 5, 2, 570000,'2018-08-20 13:43:52');
+insert into ListedHouse values (10, 0, 8, 1, 93519, 81, 4, 1, 553000,'2016-04-04 15:15:45');
+insert into ListedHouse values (11, 0, 9, 6, 44549, 02, 5, 2, 670000,'2019-03-25 13:43:57');
+insert into ListedHouse values (12, 0, 10, 1, 83519, 91, 4, 1, 553000,'2019-01-025 11:15:23');
+
+insert into Buyer values (1, 'Jin', 'Kim', 2223562889, 'melimeon@gmail.com');
+insert into Buyer values (2, 'Shiv', 'Chandaria', 432198765, 'shivchandaria@gmail.com');
+insert into Buyer values (3, 'Kavi', 'Kumar', 543987621, 'kaikumar@gmail.com');
+insert into Buyer values (4, 'Sahil', 'Shah', 954328761, 'sahilshah@gmail.com');
+insert into Buyer values (5, 'Harshil', 'Watermelon', 329876541, 'harshilwater@gmail.com');
+insert into Buyer values (6, 'Harrison', 'Mutai', 954321876, 'harrisonmutai@gmail.com');
+insert into Buyer values (7, 'Matthew', 'Hutchinson', 5423562889, 'mhutch@gmail.com');
+insert into Buyer values (8, 'Akash', 'Kai', 322198765, 'akashkai@gmail.com');
+insert into Buyer values (9, 'Thomas', 'Muler', 954987621, 'thomasmul@gmail.com');
+insert into Buyer values (10, 'Jacob', 'Taylor', 354328761, 'jacobtay@gmail.com');
+insert into Buyer values (11, 'Peter', 'Sky', 324836541, 'petersky@gmail.com');
+insert into Buyer values (12, 'Rock', 'Damon', 554221876, 'rockdamon@gmail.com');
+
+/* Each transactions here represent a house sold */
+
+/*the Sales transaction must happen on "one go" since inter-related data are being
+updated at the same time*/
+begin transaction;
+/* insert Sales information to the Sales table */
+insert into Sales values (1, 1, 5, 10, 5, 142000, '2019-02-22 16:04:09');
+/* Mark house as sold on ListedHouse */
+update ListedHouse set Sold = 1 where ListedHouseID = 10;
+end transaction;
+
+begin transaction;
+insert into Sales values (2, 2, 2, 9, 7, 928000, '2019-02-27 12:29:35');
+update ListedHouse set Sold = 1 where ListedHouseID = 9;
+end transaction;
+
+begin transaction;
+insert into Sales values (3, 3, 7, 8, 6, 452000, '2019-03-04 13:15:17');
+update ListedHouse set Sold = 1 where ListedHouseID = 8;
+end transaction;
+
+begin transaction;
+insert into Sales values (4, 4, 8, 1, 9, 872000, '2019-03-08 07:21:05');
+update ListedHouse set Sold = 1 where ListedHouseID = 1;
+end transaction;
+
+begin transaction;
+insert into Sales values (5, 5, 9, 2, 5, 1210000, '2019-03-11 05:33:14');
+update ListedHouse set Sold = 1 where ListedHouseID = 2;
+end transaction;
+
+begin transaction;
+insert into Sales values (6, 6, 6, 11, 9, 573000, '2018-09-13 12:46:56');
+update ListedHouse set Sold = 1 where ListedHouseID = 11;
+end transaction;
+
+begin transaction;
+insert into Sales values (7, 6, 4, 7, 3, 568000, '2019-03-15 12:46:56');
+update ListedHouse set Sold = 1 where ListedHouseID = 7;
+end transaction;
+
+begin transaction;
+insert into Sales values (8, 7, 3, 3, 4, 342000, '2019-03-20 16:04:09');
+update ListedHouse set Sold = 1 where ListedHouseID = 3;
+end transaction;
+
+begin transaction;
+insert into Sales values (9, 8, 2, 4, 2, 2342000, '2019-03-25 16:04:09');
+update ListedHouse set Sold = 1 where ListedHouseID = 4;
+end transaction;
+
+begin transaction;
+insert into Sales values (10, 9, 1, 6, 1, 425000, '2019-03-26 16:04:09');
+update ListedHouse set Sold = 1 where ListedHouseID = 6;
+end transaction;
+
+begin transaction;
+insert into Sales values (11, 10, 5, 5, 6, 945000, '2018-03-30 16:04:09');
+update ListedHouse set Sold = 1 where ListedHouseID = 5;
+end transaction;
+
+/* Each agents' commissions for each sales they have completed is calculated
+"at the end of the month" all at once (instead of calculating
+every sales transaction). We also calculate
+each Agent's sum total Commission and how many times they sold
+a house (represented by count commission) */
+begin transaction;
+insert into AgentCommission
+  Select AgentID,
+  /* "The estate agent commission needs to be calculated.
+  This happens on a sliding scale"*/
+    Case
+      When SalesPrice > 1000000 Then SalesPrice*0.04
+      When SalesPrice Between 100000 and 200000 Then SalesPrice*0.075
+      When SalesPrice Between 200000 and 500000 Then SalesPrice*0.06
+      When SalesPrice Between 500000 and 1000000 Then SalesPrice*0.05
+      Else SalesPrice*0.10
+    End As Commission
+  From Sales;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 1;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 2;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 3;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 4;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 5;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 6;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 7;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 8;
+
+insert into AgentCommissionSummary(TotalCommission, CountCommission)
+  select sum(Commission) as "TotalCommission",
+  count(Commission) as "CountCommission" from AgentCommission
+  where AgentID = 9;
+end transaction
